0966590af826d596b21f8adccb0580ccff8c2c17
emmanuelandyeze <3riveafrica.com>
2022-09-02 23:27:00 +0100

    editted blog post

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -113,7 +113,7 @@ Looking forward to reading more of her works.
 			  <div className="blog-cards">
 				  
 				  <div className="blog-card">
-					  <img src="/blog/2.jpeg" alt="" />
+					  <img src="/blog/5.jpeg" alt="" />
 					  {/* <h4>Uneasy lies ahead</h4> */}
 					  <p>
 						  To think of words as shapes, and feelings as lines. Or better still, to swap the both. So here I go, first it's cursive; with a smooth uninterrupted flow, displaying the thousand beauties of good penmanship. And then it's zigzag, like the unsettled heartbeats of a man in trouble. So scattered, disheveled and done in great haste. And then it is a square, being tried to fit in a round hole. And then it's the sides of a trapezium, I remember the formula ½(a+b)h?
@@ -122,7 +122,7 @@ And then it is sinusoidal as a little rhythm seems to be added. A little bit of 
 					  </p>
 				  </div>
 				  <div className="blog-card">
-					  <img src="/blog/11.jpeg" alt="" />
+					  <img src="/blog/14.jpeg" alt="" />
 					  <p>
 						  Again, the tale that has no end. Intentions long discarded, motions borne of necessity. To live without a soul, like a shoe with no sole, trudging through the mud, and having feet soggy with undesirable layers of clay. Speed slowed, deceleration so long lasting no measurements are taken. If the mind had a name, it would be the home of intentions. Intentions so deep, words long sought, yet unfound and never to be found. 
 Again, intentions long discarded, left are motions, motions in circles, in squares, in rectangles,trapeziums, and often like the zigzag motion of the mad man who has no soul. 
@@ -166,7 +166,7 @@ Till my cold nerves shiver no more.
  I love your warmth. 
 It's the place of my tranquillity.
 Again I said,
- "I'll soak in your words".
+ "I'll soak in your words"
 					  </p>
 				  </div>
 				  <div className="blog-card">
@@ -174,8 +174,8 @@ Again I said,
 					  {/* <h4>Uneasy lies the head</h4> */}
 					  <p>
 						  And if my heart be troubled, in need of some temporal calm. 
-And if my heart be bored, in need for some new scenes 
-And if need be that I receive some psychological relief, 
+And if my heart be bored, in need for some new scenes. 
+And if need be that I receive some.  psychological relief, 
 May I find them in the pages of a book. 
 
 May i flip through different scenarios, 
@@ -183,19 +183,19 @@ And live in the worlds books afford you.
 The ability to be yet not be,
 To smile in a world not yours, and be too many a character you aren't.  
 
-If perhaps I miss my friends 
-Reminiscence therefore the very haven of my emotional safety 
-May I find solace in the arms of my best reads 
+If perhaps I miss my friends. 
+Reminiscence therefore the very haven of my emotional safety. 
+May I find solace in the arms of my best reads. 
 And smile with satisfaction at the companionship of such mentally dogeared pages. 
 
 And perhaps I fall out with my love,
 And words cannot be said, and calls not be made. 
 May I fall into the arms of my best lines, 
-As I think on days when there was no love lost 
+As I think on days when there was no love lost. 
 
 If perhaps I need some calm,
 Some break from the immediate environ,
-May I sit still, and let the breeze remind me
+May I sit still, and let the breeze remind me. 
 Of a line in a book long read, whose significance is momentarily irrelevant. 
 
 If I miss home, my demised parents, and a childhood long gone. 
@@ -204,13 +204,13 @@ As my mind flips through the pages of the girl who slept on the heap of hay.
 May I smile with fulfilment, I had a good childhood, one filled with books. 
 
 And if I get weary of human interactions, 
-Needing some space for 'being' at the time being 
-May I find them on the pages of a new read, wrapped in the adventure and discovery it affords  
+Needing some space for 'being' at the time being. 
+May I find them on the pages of a new read, wrapped in the adventure and discovery it affords. 
 
 And perhaps if this poem is ever relatable,
-Of the love, relief, emotional attachment books and words have become to me
-May it deepen my love for words and quest for knowledge 
-And may I find more calm and solace on the pages of a book. 
+Of the love, relief, emotional attachment books and words have become to me. 
+May it deepen my love for words and quest for knowledge. 
+And may I find more calm and solace on the pages of a book 
 
 (Snippet from: Pass me a book,I wanna hibernate)
 					  </p>
